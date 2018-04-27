bb9c3e9da96af64e581b80a3386a444184eeacd4
Gustavo Bueno <gbueno360@gmail.com>
2018-04-27 12:04:25 -0300

    Testando Classe de Dominio

--- src/main/java/com/gustavo/cursospringboot/demo/resources/CategoryResource.java
+++ src/main/java/com/gustavo/cursospringboot/demo/resources/CategoryResource.java
@@ -1,15 +1,25 @@
 package com.gustavo.cursospringboot.demo.resources;
 
+import com.gustavo.cursospringboot.demo.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST Funcionando";
+    public List<Categoria> listar(){
+
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(new Categoria(1,"Informática"));
+        lista.add(new Categoria(2,"Escritório"));
+
+        return lista;
     }
 }
